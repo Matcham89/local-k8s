7dcd04914193c128f6738f9f3951e165f948201b
Matcham89 <cjmatcham@hotmail.co.uk>
2025-01-16 03:15:50 +0000

    refactor: change folder config

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# app.py
 from flask import Flask, render_template_string
 import os
 
@@ -8,7 +7,6 @@ app = Flask(__name__)
 def home():
     app_message = os.getenv("APP_MESSAGE", "Default Message")
     secret = os.getenv("SECRET", "Not Connected")
-    other_secret = os.getenv("OTHER_SECRET", "Not Set")
 
     # HTML Template to render in the browser
     html_template = """
@@ -17,15 +15,14 @@ def home():
     <body>
         <h1>{{ app_message }}</h1>
         <p><strong>Vault Secret:</strong> {{ secret }}</p>
-        <p><strong>Other Secret:</strong> {{ other_secret }}</p>
     </body>
     </html>
     """
-    return render_template_string(html_template, 
-                                  title="Application-A", 
-                                  app_message=app_message, 
-                                  secret=secret, 
-                                  other_secret=other_secret)
+    return render_template_string(html_template,
+                                  title="Application Title",
+                                  app_message=app_message,
+                                  secret=secret)  # Fixed syntax error here by removing the trailing comma
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)  # Fixed indentation here
+
